8f556c08ab1c3603ab1e378c81e137275517211c
Cody Moore <cody@bloomcred.it>
2019-05-01 13:45:50 -0400

    Updated aliases

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -8,5 +8,8 @@ alias gaa "git add ."
 alias gcm "git commit -m"
 alias gco "git checkout"
 alias gp "git push"
-alias gpo "git push -u origin"
+alias gpo "git push -u origin (git branch | grep \* | cut -d ' ' -f2)"
 alias clone "git clone"
+
+alias nexus-port-forward "kubectl1.8 config set-context (kubectl1.8 config current-context) --namespace=utility; kubectl1.8 port-forward (kubectl1.8 -n utility get pods -l app=sonatype-nexus -o go-template --template '{{range .items}}{{.metadata.name}}{{\"\n\"}}{{end}}' | head -n1) 8080:8081"
+
