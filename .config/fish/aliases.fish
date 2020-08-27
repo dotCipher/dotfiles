ad2da638441d0a8703ff2a8432bfa9363795cf7b
Cody Moore <cody@bloomcred.it>
2020-08-27 16:41:27 -0400

    update aliases

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -11,11 +11,17 @@ alias gp "git push"
 alias gpo "git push -u origin (git branch | grep \* | cut -d ' ' -f2)"
 alias clone "git clone"
 
+alias tf "terraform"
+alias tg "terragrunt"
+
 alias k "kubectl"
 alias kctx "kubectx"
 
-alias aws "set -x AWS_VAULT; aws-vault exec \$AWS_DEFAULT_PROFILE -- /usr/local/bin/aws $argv"
-alias avl "set -x AWS_VAULT; aws-vault login $argv"
-alias ave "set -x AWS_VAULT; aws-vault exec $argv"
+alias aws "set -e AWS_VAULT; aws-vault exec \$AWS_DEFAULT_PROFILE -- /usr/local/bin/aws $argv"
+alias avl "set -e AWS_VAULT; aws-vault login $argv"
+alias ave "set -e AWS_VAULT; aws-vault exec $argv"
+
 alias exa "exa --icons"
 alias ls "exa --icons"
+
+alias docker-nuke "docker stop (docker ps -a -q); docker rm (docker ps -a -q); docker system prune -f"
