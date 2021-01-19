d6838761f1dc6b491033a929029ad074785b4bec
Cody Moore <cody@bloomcred.it>
2021-01-19 15:58:18 -0500

    updates

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,14 +1,18 @@
 
 alias c "clear"
 
+alias ipv4 "dig +short myip.opendns.com @resolver1.opendns.com"
+
 alias gw "./gradlew"
 
 alias g "git"
 alias gaa "git add ."
+alias gc "git commit"
 alias gcm "git commit -m"
 alias gco "git checkout"
 alias gp "git push"
 alias gpo "git push -u origin (git branch | grep \* | cut -d ' ' -f2)"
+alias grhh "git reset HEAD --hard"
 alias clone "git clone"
 
 alias tf "terraform"
@@ -17,11 +21,9 @@ alias tg "terragrunt"
 alias k "kubectl"
 alias kctx "kubectx"
 
-alias aws "set -e AWS_VAULT; aws-vault exec \$AWS_DEFAULT_PROFILE -- /usr/local/bin/aws $argv"
-alias avl "set -e AWS_VAULT; aws-vault login $argv"
-alias ave "set -e AWS_VAULT; aws-vault exec $argv"
+alias avl "aws-vault login $argv"
+alias ave "aws-vault exec --no-session $argv"
 
-alias exa "exa --icons"
 alias ls "exa --icons"
 
 alias docker-nuke "docker stop (docker ps -a -q); docker rm (docker ps -a -q); docker system prune -f"
